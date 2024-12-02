ea74075171d480ad131d843ad594d6b142ccafd3
Maaxboon <maaxwelokoth@gmail.com>
2024-12-02 11:22:26 +0300

    Removes deleted files

--- src/dom-modules/toggle-visibility.js
+++
@@ -1,8 +0,0 @@
-export default function toggleVisibility(timer) {
-	const movingElements = document.querySelectorAll('.animate');
-	setTimeout(() => {
-		movingElements.forEach((element) => {
-			element.classList.toggle('show');
-		});
-	}, timer);
-}
